bb6089391ea828269b418d9dce2299baafae1b9a
daikoku <21cm0148@jec.ac.jp>
2022-09-09 09:27:22 +0900

    Initial Commit

---
+++ QuizSample/QuizSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizSampleApp.swift
+//  QuizSample
+//
+//  Created by cmStudent on 2022/09/09.
+//
+
+import SwiftUI
+
+@main
+struct QuizSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
